ceecc332a6dca6a9bbc706f77cee9816080a8a92
Song Gao <song@gao.io>
2016-04-26 20:10:06 -0500

    bug fix in chart-utils

--- chart-utils/lib/highChartTemplates.js
+++ chart-utils/lib/highChartTemplates.js
@@ -47,7 +47,7 @@ exports.area2d = (yAxisTitle) => {
     };
 
     // heuristic to detect delivery ratio charts
-    if (series[0].data[series[0].data.length/2][1] <= 1) {
+    if (series[0].data[Math.floor(series[0].data.length/2)][1] <= 1) {
       ret.yAxis.max = 1;
     }
 
